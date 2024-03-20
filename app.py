74f608e41ddca4522e27303f7552b7e9d16baef3
GOVINDFROMINDIA <govind123.ga@gmail.com>
2024-03-21 00:59:25 +0530

    Your commit message here

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, Response
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/select_image', methods=['POST'])
+def select_image():
+    if request.method == 'POST':
+        image_file = request.files['image']
+        if image_file:
+            image = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
+            # Your image processing code here
+
+            return f'Success! Image dimensions: {image.shape[1]}x{image.shape[0]}'
+        else:
+            return 'No image selected'
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
